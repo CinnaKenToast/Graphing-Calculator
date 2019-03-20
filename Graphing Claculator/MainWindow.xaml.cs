ad7a230ff7bd57759dcafbbae14099c8cfdae292
daniel <deckert2@my.apsu.edu>
2019-03-20 08:20:43 -0500

    linked the projects

--- Graphing Claculator/MainWindow.xaml.cs
+++ Graphing Claculator/MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+//how we get to to talk to our libary
+using CalcLib;
+
 namespace Graphing_Claculator
 {
     /// <summary>
