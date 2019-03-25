0f78327c5b9caf732bbf4cc3ad07dd3d815bd0f5
Ken Shipley <rockmankim@gmail.com>
2019-03-25 01:12:31 -0500

    Added more expressions

--- CalcLib/Expression.cs
+++ CalcLib/Expression.cs
@@ -45,4 +45,92 @@ namespace CalcLib
             return (double)number;
         }
     }
+
+    class AdditionExpression : BinaryExpression
+    {
+        protected override double evaluate(double left, double right)
+        {
+            return left + right;
+        }
+    }
+
+    class MinusExpression : BinaryExpression
+    {
+        protected override double evaluate(double left, double right)
+        {
+            return left - right;
+        }
+    }
+
+    class MultiplyExpression : BinaryExpression
+    {
+        protected override double evaluate(double left, double right)
+        {
+            return left * right;
+        }
+    }
+
+    class DivideExpression : BinaryExpression
+    {
+        protected override double evaluate(double left, double right)
+        {
+            return left / right;
+        }
+    }
+
+    class ExpononetExpression : BinaryExpression
+    {
+        protected override double evaluate(double left, double right)
+        {
+            return Math.Pow(left,right);
+        }
+    }
+
+    class LogarithmExpression : IExpression
+    {
+        private IExpression operand;
+
+        public LogarithmExpression(IExpression operand)
+        {
+            this.operand = operand;
+        }
+
+        public double evaluate()
+        {
+            return Math.Log10(operand.evaluate());
+        }
+    }
+
+    class NegateExpresion : IExpression
+    {
+        private IExpression operand;
+        public NegateExpresion(IExpression operand)
+        {
+            this.operand = operand;
+        }
+
+        public double evaluate()
+        {
+            return -operand.evaluate();
+        }
+    }
+
+    class FactorialExpresion : IExpression
+    {
+        private double operand;
+        public FactorialExpresion(int operand)
+        {
+            this.operand = operand;
+        }
+
+        public double evaluate()
+        {
+            double ans = 1;
+            for(double i = operand; i > 1; i--)
+            {
+                ans = ans * i;
+            }
+            return ans;
+        }
+    }
 }
